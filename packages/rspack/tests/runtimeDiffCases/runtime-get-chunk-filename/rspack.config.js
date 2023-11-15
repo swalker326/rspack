9a4440b1ea7360112b62f303c701f949d05b36f9
LingyuCoder <--global>
2023-11-15 14:28:12 +0800

    chore(runtime-diff): alignment of get chunk name runtime module

--- packages/rspack/tests/runtimeDiffCases/runtime-get-chunk-filename/rspack.config.js
+++ packages/rspack/tests/runtimeDiffCases/runtime-get-chunk-filename/rspack.config.js
@@ -1,20 +1,6 @@
 module.exports = {
 	output: {
-		chunkFilename: "[name].[chunkhash:4].js",
+		chunkFilename: "[name].[fullhash:4].js",
 		cssChunkFilename: "[name].css"
-	},
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				a: {
-					test: /a.js/,
-					minSize: 0
-				},
-				b: {
-					test: /b.js/,
-					minSize: 0
-				}
-			}
-		}
 	}
 };
--- packages/rspack/tests/runtimeDiffCases/runtime-get-chunk-filename/webpack.config.js
+++ packages/rspack/tests/runtimeDiffCases/runtime-get-chunk-filename/webpack.config.js
@@ -1,22 +1,6 @@
 module.exports = {
 	output: {
-		chunkFilename: "[name].[chunkhash:4].js",
+		chunkFilename: "[name].[fullhash:4].js",
 		cssChunkFilename: "[name].css"
-	},
-	optimization: {
-		// splitChunks: {
-		//   cacheGroups: {
-		//     a: {
-		//       filename: 'a.[contenthash:4].js',
-		//       test: /a.js/,
-		//       minSize: 0,
-		//     },
-		//     b: {
-		//       filename: 'b.[contenthash:4].js',
-		//       test: /b.js/,
-		//       minSize: 0,
-		//     }
-		//   },
-		// }
 	}
 };
